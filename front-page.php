c4af3c48b78fc1361c3d3983df312db5cfc04833
R-P17 <49292213+R-P17@users.noreply.github.com>
2021-12-28 00:39:48 +0200

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -8,11 +8,16 @@
 	</div>
 <div class="container">
 	<div class="row">
-		<div class="col-sm-6" >
+			<span class="d-block d-sm-none">
+			<img src="<?php echo get_template_directory_uri() ?>/img/tech.jpg" class="img-thumbnail" alt="test1" >
+			</span>
+		<div class="col-sm-6" id="text-lorem">
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s </p>
 		</div>
 		<div class="col-sm-6">
+			<span class="d-none d-sm-block">
 			<img src="<?php echo get_template_directory_uri() ?>/img/tech.jpg" class="img-thumbnail" alt="test1" >
+			</span>
 		</div>
 
 </div>
@@ -26,11 +31,16 @@
 		</div>
 			
 	<div class="row">
+		<span class="d-block d-sm-none">
+			<img src="<?php echo get_template_directory_uri() ?>/img/tablet.jpg" class="img-thumbnail" alt="test3" >
+		</span>		
 		<div class="col-sm-6" id="text_lorem2">
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s </p>
 		</div>
 		<div class="col-sm-6">
+			<span class="d-none d-sm-block">
 			<img src="<?php echo get_template_directory_uri() ?>/img/tablet.jpg" class="img-thumbnail" alt="test3" >
+			</span>
 		</div>
 			
 	<div class="row">
