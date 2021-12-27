9e6ca3109e7b6e6589c0668c727e1dbe9ff0198b
R-P17 <49292213+R-P17@users.noreply.github.com>
2021-12-27 03:11:49 +0200

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -8,38 +8,36 @@
 	</div>
 <div class="container">
 	<div class="row">
-		<div class="col" bt>
+		<div class="col-sm-6" >
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s </p>
 		</div>
-		<div class="col">
+		<div class="col-sm-6">
 			<img src="<?php echo get_template_directory_uri() ?>/img/tech.jpg" class="img-thumbnail" alt="test1" >
 		</div>
 
 </div>
-<div class="container">
 
-</div>
 	<div class="row">
-		<div class="col">
+		<div class="col-sm-6">
 			<img src="<?php echo get_template_directory_uri() ?>/img/man.jpg" class="img-thumbnail" alt="test2" >
 		</div>
-		<div class="col" id="text_lorem1">
+		<div class="col-sm-6" id="text_lorem1">
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s </p>
 		</div>
 			
 	<div class="row">
-		<div class="col" id="text_lorem2">
+		<div class="col-sm-6" id="text_lorem2">
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s </p>
 		</div>
-		<div class="col">
+		<div class="col-sm-6">
 			<img src="<?php echo get_template_directory_uri() ?>/img/tablet.jpg" class="img-thumbnail" alt="test3" >
 		</div>
 			
 	<div class="row">
-		<div class="col">
+		<div class="col-sm-6">
 			<img src="<?php echo get_template_directory_uri() ?>/img/digital.jpg" class="img-thumbnail" alt="test4" >
 		</div>
-		<div class="col">
+		<div class="col-sm-6">
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s </p>
 		</div>
 </div>
